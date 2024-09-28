a41d272aff44a7b42ae6dcfa101a6cd2d70d8a0a
One290 <lenya.dorovskoy@mail.ru>
2024-09-28 17:10:22 +0800

    fnjhvh

--- DeployToDockerImage.sh
+++ DeployToDockerImage.sh
@@ -4,7 +4,7 @@ if [ -n "$1" ]
 then
   echo 'Starting building Docker image. Image name: "service1"'
   fullPath=$(realpath $1)
-  docker build -t service1 --build-arg SERVICE_DIRECTORY_PATH=$fullPath Web
+  docker build -f Web -t service1 --build-arg SERVICE_DIRECTORY_PATH=$fullPath
 else
   echo 'The output directory path argument is mandatory.'
 fi
