e2bd68baa3cdd0046e34f3fdbb4e8f19c7801bfe
One290 <lenya.dorovskoy@mail.ru>
2024-09-28 16:14:53 +0800

    kgdy

--- Build.sh
+++ Build.sh
@@ -3,7 +3,6 @@
 if [ -n "$1" ]
 then
   dotnet publish Web/Web.csproj --configuration Release --runtime linux-x64 --self-contained true --framework net8.0 --output $1 -p:PublishReadyToRun=true
-fi
 else
   echo 'The output directory path argument is mandatory.'
 fi
--- DeployToDockerImage.sh
+++ DeployToDockerImage.sh
@@ -4,7 +4,6 @@ if [ -n "$1" ]
 then
   echo 'Starting building Docker image. Image name: "service1"'
   docker build -t service1 --build-arg SERVICE_DIRECTORY_PATH=$1 Web
-fi
 else
   echo 'The output directory path argument is mandatory.'
 fi
